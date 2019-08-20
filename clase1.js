d46ec30b09cc2bb772a8ab1f17309072e3b308e2
martino2197 <martino2197@gmail.com>
2019-08-20 11:57:21 -0500

    Primera clase (Variables)

---
+++ clase1.js
@@ -0,0 +1,16 @@
+//JS es debilmente tipado, no hay nada que diga que la Variable
+//texto sea de un tipo definido
+//console.log('Hola mundo JS!')
+//var nombre
+//nombre = 'Martin'
+
+//var nombre = 'Martin'
+//var apellido = 'Maceda'
+var nombre = 'Martin', apellido = 'Maceda'
+var edad = 28
+
+edad = '28 anos'
+console.log('Hola ' + nombre + ' ' + apellido)
+console.log('Tengo ' + edad)
+
+var peso = 75
