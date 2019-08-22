0cb44cdf09528304d185e9cec8ccebcf8db09c9b
martino2197 <martino2197@gmail.com>
2019-08-22 13:09:19 -0500

    Cuarta clase (Funcioes y parametros)

---
+++ clase4.js
@@ -0,0 +1,9 @@
+var nombre = 'Martin', edad = 28
+
+ function imprimirEdad(n, e){
+   console.log(`${n} tiene ${e} anos`)
+ }
+
+imprimirEdad(nombre, edad)
+imprimirEdad('Ricks', 23)
+imprimirEdad('11', 13)
