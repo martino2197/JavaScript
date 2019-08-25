5b9ecc87a587a2f0518c7af6b99cd6f7958ce463
martino2197 <martino2197@gmail.com>
2019-08-24 23:05:18 -0500

    Novena clase (Comparaciones JavaScript; Objetos y Datos Primitivos)

---
+++ clase9.js
@@ -0,0 +1,39 @@
+var x = 4, y = '4'
+//x == y
+//truex == y
+//x === y
+//false
+// SIEMPRE UTILIZAR EL TRIPLE IGUAL
+
+var Martin = {
+  nombre: 'Martin'
+}
+
+var otraPersona = {
+  nombre: 'Martin'
+}
+//otraPersona
+//{nombre: "Arturo"}
+//Martin == otraPersona
+//false
+
+var otraPersona2 = Martin
+//Martin === otraPersona2
+//true
+
+//objeto literal, estamos creando un objeto nuevos
+//var otraPersona2 = {
+  //...Martin
+//}
+//Martin == otraPersona2
+//false
+
+//Martin
+//{nombre: "Martin"}
+//otraPersona2
+//{nombre: "Martin"}
+//otraPersona2.nombre = 'Arturo"
+//otraPersona2.nombre = "Arturo"
+//"Arturo"
+//Martin
+//{nombre: "Arturo"}
