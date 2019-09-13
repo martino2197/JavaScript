a284a0e6b1ff90a5ea6995a9b7d78c8cbb1581f5
martino2197 <martino2197@gmail.com>
2019-09-13 16:35:15 -0500

    Veintidos y Veintitres clase (Arrow function para los prototipos de clases: El contexto de las funciones y quien es 'this.')

--- clase22.js
+++ clase22.js
@@ -9,29 +9,22 @@ function persona(nombre, apellido, estatura) {
   // esta implicito que se retorna el objeto: return this
 }
 
-//creamo la funcion saludar para nuestro prototipo
-persona.prototype.saludar = function (){
+//Las arrow funtion tienen el detalle de que cambian el contexto
+//para this, pasa a ser el contexto de window("la clase global")
+//es por ello que nos encontramos con errores de undefined para thiss
+persona.prototype.saludar = () => {
   console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`);
 }
 
-persona.prototype.soyAlto = function (){
-  if (this.estatura > 1.70) {
-      console.log(`Soy Alto por que mido ${this.estatura}`)
-  } else {
-    console.log(`soy chaparro`);
-}
+persona.prototype.soyAlto = () => {
+  debugger
+  return this.estatura > 1.70
 }
 //Persona.prototype.soy//Alto = function () {
 //    (this.altura >= 1.8) ? console.log('es Alto') : console.log('es bajo');
 //}
 
 var Martin = new persona('Martin', 'Maceda', 1.72)
-//new sirve para crear el nuevo objeto y a ese objeto se le asigna el prototipo
-Martin.saludar() //Hola me llamo Martin
 var Salma = new persona('Salma', 'lovewood', 1.60)
 var Arturo = new persona('Arturo', 'Rosas', 1.78)
-
-Arturo.saludar()
-
-Martin.soyAlto()
-Salma.soyAlto()
+//new sirve para crear el nuevo objeto y a ese objeto se le asigna el prototipo
